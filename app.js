d57f061c5ed805bd022490a60379d378f6bde64a
WARDOG080 <59506904+WARDOG080@users.noreply.github.com>
2023-12-03 16:32:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
 
 var app = express();
-
-var helloRouter = require('./routes/hello');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use ('/hello', helloRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

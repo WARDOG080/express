3c3a57c50386672c453e7a44d02e315931a387f3
WARDOG080 <59506904+WARDOG080@users.noreply.github.com>
2023-12-03 19:47:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
-var catRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
 
 var app = express();
 
